3cb9d49863830acea699c23b6b09e1faa49bd44c
EMorenoH <68623282+EMorenoH@users.noreply.github.com>
2021-02-20 08:51:35 -0600

    Add files via upload

---
+++ templates/pie.php
@@ -0,0 +1,13 @@
+<footer>
+	<hr>
+	<div class="">Derechos Reservados Veterinaria LPE 2021</div>
+	
+</footer>
+</body>
+
+	<script src="https://cdn.jsdelivr.net/npm/glider-js@1.7.3/glider.min.js"></script>
+	<script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
+	<script src="js/app.js"></script>
+</div>
+
+</html>
